2b06b708b8131d3a05dd9304ab25ffcba15f1f54
Mateus Barbosa <72516703+mateuscbarbosa@users.noreply.github.com>
2021-10-15 12:14:09 -0300

    Testes da classe UsuarioController

---
+++ src/test/java/br/com/alura/carteira/controller/UsuarioControllerTest.java
@@ -0,0 +1,51 @@
+package br.com.alura.carteira.controller;
+
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Transactional
+class UsuarioControllerTest {
+
+	@Autowired
+	private MockMvc mvc;
+	
+	@Test
+	void naoDeveriaCadastrarUsuarioComDadosIncompletos() throws Exception {
+		String json = "{}";
+		
+		mvc.perform(MockMvcRequestBuilders
+				.post("/usuarios")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json))
+			.andExpect(MockMvcResultMatchers.status().isBadRequest());
+	}
+	
+	@Test
+	void deveriaCadastrarUsuarioComDadosCompletos() throws Exception {
+		String json = "{\"nome\": \"teste\", \"login\": \"teste@email.com\"}";
+		
+		mvc.perform(MockMvcRequestBuilders
+				.post("/usuarios")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json))
+			.andExpect(MockMvcResultMatchers.status().isCreated())
+			.andExpect(MockMvcResultMatchers.header().exists("Location"))
+			.andExpect(MockMvcResultMatchers.content().json(json));
+	}
+
+}
