f1e2ce97b7ff6327c13f29e744dbcdf5396104d2
Mateus Barbosa <72516703+mateuscbarbosa@users.noreply.github.com>
2021-10-27 17:21:42 -0300

    Validação do token via Filter nas requisições

--- src/main/java/br/com/alura/carteira/infra/security/TokenService.java
+++ src/main/java/br/com/alura/carteira/infra/security/TokenService.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import br.com.alura.carteira.modelo.Usuario;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -19,8 +20,31 @@ public class TokenService {
 		
 		return Jwts
 				.builder()
-				.setId(logado.getId().toString())
+				.setSubject(logado.getId().toString())
 				.signWith(SignatureAlgorithm.HS256, secret)
 				.compact();
 	}
+	
+	public boolean eValido(String token) {
+		try {
+			Jwts
+			 .parser()
+			 .setSigningKey(secret)
+			 .parseClaimsJws(token);
+			return true;
+		} catch(Exception e) {
+			return false;
+		}
+	}
+
+	public Long extrairIdUsuario(String token) {
+		Claims claims = Jwts
+		 .parser()
+		 .setSigningKey(secret)
+		 .parseClaimsJws(token)
+		 .getBody();
+		
+		return Long.parseLong(claims.getSubject());
+	}
+	
 }
